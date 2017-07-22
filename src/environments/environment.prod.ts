3b3b2f909798a86f79272485617217bb3e171f53
Ade A <AdeAkinyooye@gmail.com>
2017-07-22 14:22:37 +0100

    oops

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiServerBase: "http://ec2-35-176-19-95.eu-west-2.compute.amazonaws.com:8080/api/v1/"
+  apiServerBase: "http://ec2-35-176-19-95.eu-west-2.compute.amazonaws.com/api/v1/"
 };
